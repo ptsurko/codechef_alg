3fec92aa5c51046b18ff8c23f48090b4841f4aa7
ptsurko <pavel.tsurko@gmail.com>
2014-03-28 01:29:03 -0700

    added more solutions

--- MARCH14/BINTOUR/Main.java
+++ MARCH14/BINTOUR/Main.java
@@ -7,7 +7,7 @@ import java.io.PrintWriter;
 
 public class Main {
 	private static int Module = 1000000009;
-	
+	private static long[] fact = new long[(int)Math.pow(2, 19)];
 	public static void main(String[] args) throws Exception, Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
@@ -15,6 +15,7 @@ public class Main {
 		int roundCount = Integer.parseInt(br.readLine());
 		
 		int participantCount = (int)Math.pow(2, roundCount);
+		prepareFactorials();
 		
 		for(int index = 0; index < participantCount; index++) {
 			out.println(getConfiurationCount(index, participantCount));
@@ -23,19 +24,28 @@ public class Main {
 		out.flush();
 		out.close();
 	}
+	
+	public static void prepareFactorials() {
+		long prev = 1;
+		fact[0] = prev;
+		for(int i = 1; i < fact.length; i++) {
+			fact[i] = fact[i - 1] * i % Module;
+		}
+	}
 
 	public static long getConfiurationCount(int index, int participantCount) {
 		if (index + 1 < participantCount / 2) {
 			return 0;
 		} else if (index + 1 == participantCount) {
-			return factorial(index + 1);
+			return fact[index + 1];
 		} else if (index + 1 >= participantCount / 2) {
-			long fact = factorial(participantCount / 2);
-			long result = fact * fact % Module;
-			result *= 2;
-			if (index + 1 > participantCount / 2) {
-				result *= index;
+			long result = fact[participantCount / 2];
+			result = multiply(result, participantCount / 2, Module);
+			for (int i = index; i >= (index + 1) - participantCount / 2 + 1; i--) {
+				result = multiply(result, i, Module);
 			}
+			
+			result = multiply(result, 2, Module);
 			result %= Module;
 			
 			return result;
@@ -44,11 +54,20 @@ public class Main {
 		return 0;
 	}
 	
-	private static long factorial(int number) {
-		long result = 1;
-		for(int i = 2; i <= number; i++) {
-			result = (result * i) % Module;
+	public static long multiply(long a, long b, long module) {
+		if (a == 0 || b == 0) {
+			return 0;
 		}
-		return result;
+		
+		long temp = a, result = 0;
+		while(b > 1) {
+			if (b % 2 == 1) {
+				result = (result + temp) % module;
+			}
+			temp = (temp << 1) % module;
+			b = b >> 1;
+		}
+		
+		return (result + temp) % module;
 	}
 }
--- MARCH14/BINTOUR/TournamentTest.java
+++ MARCH14/BINTOUR/TournamentTest.java
@@ -5,6 +5,8 @@ import org.junit.*;
 public class TournamentTest {
 	@Test
 	public void testConfiguration() {
+		Main.prepareFactorials();
+		
 		Assert.assertEquals(Main.getConfiurationCount(0, 4), 0);
 		
 		Assert.assertEquals(Main.getConfiurationCount(0, 2), 2);
@@ -26,5 +28,28 @@ public class TournamentTest {
 		Assert.assertEquals(Main.getConfiurationCount(6, 8), 23040);
 		
 		Assert.assertEquals(Main.getConfiurationCount(7, 8), 40320);
+		
+		Assert.assertEquals(Main.getConfiurationCount(7, 16), 251404773);
+
+		Assert.assertEquals(Main.getConfiurationCount(9, 16), 50571747);
+		
+		Assert.assertEquals(Main.getConfiurationCount(10, 16), 168572490);
+	}
+	
+	@Test
+	public void multiplyTest() {
+		Assert.assertEquals(Main.multiply(2, 2, 1000), 4);
+		
+		Assert.assertEquals(Main.multiply(2, 3, 1000), 6);
+		
+		Assert.assertEquals(Main.multiply(2, 2, 2), 0);
+		
+		
+		Assert.assertEquals(Main.multiply(2, 1, 1000), 2);
+		
+		Assert.assertEquals(Main.multiply(2, 0, 1000), 0);
+		
+		
+		Assert.assertEquals(Main.multiply(3, 7, 1000), 21);
 	}
 }
